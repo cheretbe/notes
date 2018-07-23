dd5879927a2fdf4d4527a87e992328ef3ae089d4
cheretbe <cheretbe@users.noreply.github.com>
2018-07-23 08:54:56 +0200

    Update git_intial_setup.sh

--- files/git/git_intial_setup.sh
+++ files/git/git_intial_setup.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+echo "Setting git parameters"
+
 git config --global user.useConfigOnly true
 
 # [!!] This will make git to store passwords in plaintext
 git config --global credential.helper store
 
 git config --global push.default simple
+
+echo "Done"
