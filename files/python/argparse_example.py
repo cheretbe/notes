8b1710428d7a2df0d21ccfac374b14397f737634
cheretbe <cheretbe@users.noreply.github.com>
2018-12-04 10:32:00 +0200

    Update argparse_example.py

--- files/python/argparse_example.py
+++ files/python/argparse_example.py
@@ -9,10 +9,13 @@ parser.add_argument("optional_arg_1", nargs="?", default="", help="Optional argu
 parser.add_argument("optional_arg_2", nargs="?", default="", help="Optional argument 2")
 
 parser.add_argument("-v", "--verbosity", type=int, choices=[0, 1, 2], help="increase output verbosity")
+
 parser.add_argument('-d', '--dont-do-something', dest='dont_do_something', action='store_true',
     default=False, help='Do not do something')
 parser.add_argument('--dont-do-something', dest='dont_do_something', action='store_true',
     default=False, help='Do not do something')
+
+parser.add_argument("--required-option", required=True, help="This option is required")
 
 # Change *display* name of an argument
 # Here it will display
