aee5329693cb112d31bb39256b9191e3b80b6dae
cheretbe <a.a.orlov@github.com>
2023-10-21 23:38:57 +0200

    Add scripts/icecast-metadata

---
+++ scripts/icecast-metadata/README.md
@@ -0,0 +1,13 @@
+* https://github.com/dirble/streamscrobbler-python/tree/master
+* There is a Python3 fork: https://github.com/andreztz/streamscrobbler-python
+    * https://pypi.org/project/streamscrobbler3/
+    * Doesn't seem to work(`get_server_info()` returns nothing)
+
+So we just stick with Python2 version in Docker:
+```shell
+docker run --rm -it python:2.7.18-stretch bash
+docker run -v ./absolutechillout:/data --rm -it python:2.7.18-stretch bash
+pip install httplib2  streamscrobbler@git+https://github.com/dirble/streamscrobbler-python
+git clone https://github.com/cheretbe/notes.git
+notes/scripts/icecast-metadata/get_metadata.py /data/absolutechillout.com_tracks.txt
+```
---
+++ scripts/icecast-metadata/get_metadata.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+import datetime
+import argparse
+import random
+import time
+import contextlib
+from streamscrobbler import streamscrobbler
+
+@contextlib.contextmanager
+def redirect_stdout():
+    original = sys.stdout
+    try:
+        sys.stdout = open(os.devnull, 'w')
+        yield
+    finally:
+        sys.stdout.close()
+        sys.stdout = original
+
+def get_current_track():
+    # return(random.choice(["track1", "track2", "track3", "track4"]))
+    with redirect_stdout():
+        return streamscrobbler().getServerInfo("http://streaming.live365.com/b05055_128mp3").get("metadata")["song"].strip()
+
+def add_item(item, file_name):
+    item += "\n"
+    with open(file_name, "a+") as f:
+        f.seek(0)
+        lines = f.readlines()
+        if item not in lines:
+            print(" writing " + item.rstrip("\n"))
+            lines.append(item)
+            lines.sort(key=lambda s: s.lower())
+            f.seek(0)
+            f.truncate()
+            f.writelines(lines)
+
+def main(args):
+    previous_track = ""
+    while True:
+        track = get_current_track()
+        print(datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S ") + track)
+        if track != previous_track:
+            print(" trying to write " + track)
+            add_item(track, args.out_file)
+        previous_track = track
+        time.sleep(60)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("out_file", help="Output file")
+    args = parser.parse_args()
+    main(args)
