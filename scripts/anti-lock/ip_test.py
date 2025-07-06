1807ff807cfaf925bb34f32e08577336eacbdd04
cheretbe <a.a.orlov@github.com>
2025-07-06 11:26:09 +0200

    Move python examples

--- scripts/anti-lock/ip_test.py
+++ scripts/anti-lock/ip_test.py
@@ -2,24 +2,54 @@
 
 import argparse
 import ipaddress
+import requests
 
 # dig +short ya.ru | grep -v '\.$' | xargs -I % scripts/anti-lock/ip_test.py %
 
-def main(args):
+
+def main1(args):
     # curl https://stat.ripe.net/data/country-resource-list/data.json?resource=RU | jq -r '.data.resources.ipv4 | .[]' > /tmp/ru_ip_list.txt
     with open("/tmp/ru_ip_list.txt", "r") as f:
         for line in f:
             ip_range = line.rstrip()
             if "-" in ip_range:
                 first_ip, last_ip = ip_range.split("-")
-                ip_networks = list(ipaddress.summarize_address_range(
-                    first=ipaddress.IPv4Address(first_ip), last=ipaddress.IPv4Address(last_ip)
-                ))
+                ip_networks = list(
+                    ipaddress.summarize_address_range(
+                        first=ipaddress.IPv4Address(first_ip), last=ipaddress.IPv4Address(last_ip)
+                    )
+                )
             else:
                 ip_networks = [ipaddress.ip_network(ip_range)]
             for ip_network in ip_networks:
                 if ipaddress.ip_address(args.ip) in ip_network:
                     print(f"{args.ip} is in {ip_network} network")
+
+
+def main(args):
+    print("Reading IP list")
+
+    ip_list = []
+    for country_code in ("RU", "BY"):
+        response = requests.get(
+            f"https://stat.ripe.net/data/country-resource-list/data.json?resource={country_code}"
+        ).json()
+        ip_list += response["data"]["resources"]["ipv4"]
+
+    for ip_range in ip_list:
+        if "-" in ip_range:
+            first_ip, last_ip = ip_range.split("-")
+            ip_networks = list(
+                ipaddress.summarize_address_range(
+                    first=ipaddress.IPv4Address(first_ip), last=ipaddress.IPv4Address(last_ip)
+                )
+            )
+        else:
+            ip_networks = [ipaddress.ip_network(ip_range)]
+        for ip_network in ip_networks:
+            if ipaddress.ip_address(args.ip) in ip_network:
+                print(f"{args.ip} is in {ip_network} network")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
